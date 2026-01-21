465a9047aefabd67824747bb04659d1c21b75cdf
0xshikhar <64684053+0xshikhar@users.noreply.github.com>
2026-01-21 18:52:02 +0530

    feat: Implement environment variable utility

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from '../prisma/generated/client'
 import { PrismaPg } from "@prisma/adapter-pg"
+import { requireEnv } from "@/lib/env";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: requireEnv("DATABASE_URL") });
 const prisma = new PrismaClient({ adapter });
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma }
